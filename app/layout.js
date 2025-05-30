87c7f7378bf2db8192875d1787918eaae63bb72c
devheris <badairoinioluwa578@gmail.com>
2025-05-30 19:08:23 +0100

    Day 3 commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,30 @@
-import Navigation from "./components/Navigation";
+import { Jost } from "next/font/google";
+
+const jost = Jost({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Navigation />
-        {children}
+    <html lang="en" className={jost.className}>
+      <body className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
